4960bf57dd178d290781bd0351aa84e0625a6e81
Anurag kumar <anuragkumar1252003@gmail.com>
2026-07-20 23:57:23 +0530

    feat: update image reference to use apple-touch-icon in routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -101,7 +101,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
       { rel: "icon", href: "/image.png", type: "image/png" },
-      { rel: "image", href: "/image.png" },
+      { rel: "apple-touch-icon", href: "/image.png" },
       { rel: "manifest", href: "/manifest.webmanifest" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
